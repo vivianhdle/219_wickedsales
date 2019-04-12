3e075e183baf7eb4a6bf9d1705d5c3038a2a52d7
Vivian Le <vivianhdle@gmail.com>
2019-04-12 11:37:03 -0700

    added functionality into the cart icon, modal, and routes

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -3,13 +3,19 @@ import './modal.scss'
 
 class Modal extends Component{
     render(){
-        if(this.props.isOpen){
+        const {isOpen,defaultAction,children,defaultActionText='Okay',secondaryAction=null,secondaryActionText='Cancel'}=this.props
+        if(isOpen){
             return (
                 <div className="ws-modal">
                     <div className="ws-modal-content">
-                        {this.props.children}
+                        {children}
                         <div className="ws-modal-actions center">
-                            <button onClick={this.props.close} className="btn btn-large teal darken-2">Okay</button>
+                            <button onClick={defaultAction} className="btn btn-large teal darken-2">{defaultActionText}
+                            </button>
+                            {secondaryAction
+                                    ? <button onClick={secondaryAction} className="btn btn-large teal darken-2">{secondaryActionText}</button>
+                                    :null
+                            }
                         </div>
                     </div>
                 </div>
--- src/components/products/product_add.js
+++ src/components/products/product_add.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import { withRouter } from 'react-router-dom';
-import Modal from '../modal'
+import Modal from '../modal';
+import {formatMoney} from '../../helpers'
 
 class ProductAdd extends Component{
     constructor(props){
@@ -13,14 +14,14 @@ class ProductAdd extends Component{
             cartQty:0
         }
         this.addToCart=this.addToCart.bind(this);
+        this.closeModal=this.closeModal.bind(this);
+        this.goToCart = this.goToCart.bind(this);
     }
     addToCart(){
         const {productId,updateCart}=this.props
         const {qty} = this.state
         axios.get(`/api/addcartitem.php?product_id=${productId}&quantity=${qty}`).then((resp)=>{
-            // console.log('Add Cart Resp');
             const {cartCount,cartTotal}=resp.data;
-            // this.props.history.push('/cart');
             updateCart(cartCount);
             this.setState({
                 'modalOpen':true,
@@ -41,6 +42,15 @@ class ProductAdd extends Component{
             })
         }
     }
+    closeModal(){
+        this.setState({
+            modalOpen:false,
+            qty:1
+        })
+    }
+    goToCart(){
+        this.props.history.push('/cart');
+    }
     render(){
         const {modalOpen,cartQty,totalPrice,qty} = this.state;
 
@@ -53,15 +63,15 @@ class ProductAdd extends Component{
                     <button className="btn btn-floating green lighten-3 btn-small" onClick={this.incrementQty}><i className="material-icons">add</i></button>
                 </span>
                 <button className="green lighten-3 btn" onClick={this.addToCart}><i className="material-icons">add_shopping_cart</i></button>
-                <Modal isOpen={modalOpen}>
-                    <h1 className="center">{qty} item(s) Added to Cart</h1>
+                <Modal isOpen={modalOpen} defaultAction={this.closeModal} defaultActionText="Continue Shopping" secondaryAction= {this.goToCart} secondaryActionText="View Cart">
+                    <h1 className="center">{qty} Item{qty>1 && 's'} Added to Cart</h1>
                     <div className="row">
-                        <div className="col s6">Cart Total Items</div>
-                        <div className="col s6">{cartQty}</div>
+                        <div className="col s6">Cart Total Items:</div>
+                        <div className="col s6 left-align">{cartQty}</div>
                     </div>
                     <div className="row">
-                        <div className="col s6">Cart Total Price</div>
-                        <div className="col s6">{totalPrice}</div>
+                        <div className="col s6">Cart Total Price:</div>
+                        <div className="col s6 left-align">{formatMoney(totalPrice)}</div>
                     </div>
                 </Modal>
             </div>
